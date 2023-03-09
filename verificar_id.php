f1303bf651403ee2a24f8394e6cb0094a020d362
ulisseeees <fariaulisses88@gmail.com>
2023-03-09 08:56:51 -0300

    sexo

--- verificar_id.php
+++ verificar_id.php
@@ -1,36 +1,48 @@
 <?php
 
-if(isset($_POST['partida_id'])) {
-    $partida_id = $_POST['partida_id'];
-
-    // Conectar ao banco de dados MySQL
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "competitivematches";
-    
-    // Cria a conexão
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-    
-    // Verifica se a conexão foi bem sucedida
-    if (!$conn) {
-        die("Conexão falhou: " . mysqli_connect_error());
-    }
-    
-    // Executa a query para verificar se a partida existe
-    $sql = "SELECT * FROM partidas WHERE id = '$partida_id'";
-    $result = mysqli_query($conn, $sql);
-    
-    // Verifica se a query retornou resultados
-    if (mysqli_num_rows($result) > 0) {
-        // A partida existe
-        echo "Partida encontrada!";
-    } else {
-        // A partida não existe
-        echo "Partida não encontrada!";
-    }
-    
-    // Fecha a conexão
-    mysqli_close($conn);
+// Verifica se o método de requisição é POST
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  
+  // Obtém o ID da partida enviado via POST
+  $partida_id = $_POST['partida-id'];
+  
+  // Abre a conexão com o banco de dados MySQL
+  $conexao = mysqli_connect('localhost', 'root', '', 'competitivematches');
+  
+  // Verifica se a conexão foi bem sucedida
+  if (!$conexao) {
+    die('Erro ao conectar ao banco de dados');
+  }
+  
+  // Prepara a consulta SQL para verificar se o ID da partida existe na tabela "partidas"
+  $consulta = mysqli_prepare($conexao, "SELECT COUNT(*) FROM partidas WHERE id = ?");
+  
+  // Vincula o ID da partida à consulta SQL
+  mysqli_stmt_bind_param($consulta, 's', $partida_id);
+  
+  // Executa a consulta SQL
+  mysqli_stmt_execute($consulta);
+  
+  // Obtém o resultado da consulta SQL
+  mysqli_stmt_bind_result($consulta, $resultado);
+  
+  // Lê o resultado da consulta SQL
+  mysqli_stmt_fetch($consulta);
+  
+  // Verifica se o resultado é maior que 0 (ou seja, se o ID da partida existe)
+  if ($resultado > 0) {
+    // Redireciona o usuário para a página "partida.html"
+    header('Location: partida.html');
+    exit;
+  } else {
+    // Exibe uma mensagem de erro
+    echo 'O ID da partida informado não existe.';
+  }
+  
+  // Fecha a conexão com o banco de dados MySQL
+  mysqli_close($conexao);
+  
+} else {
+  // Se o método de requisição não for POST, exibe uma mensagem de erro
+  echo 'Método de requisição inválido.';
 }
-?>
