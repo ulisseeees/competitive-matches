b5e08e43fc999330f265b9c88e708ae6bfc69be0
ulisseeees <fariaulisses88@gmail.com>
2023-03-09 07:53:47 -0300

    sexo

--- public/script.js
+++ public/script.js
@@ -1,74 +1,41 @@
-const urlParams = new URLSearchParams(window.location.search);
-const partidaId = urlParams.get('partidaId');
-
-if (!partidaId) {
-  window.location.href = 'index.html';
-}
-
-const socket = io();
-const nomeInput = document.getElementById('nome');
-const mensagemInput = document.getElementById('mensagem');
-const enviarBtn = document.getElementById('enviar');
-const mensagensContainer = document.getElementById('mensagens-container');
-const listaJogadores = document.getElementById('lista-jogadores');
-
-let nome = '';
-
-function adicionarMensagem(mensagem) {
-  const mensagemElement = document.createElement('div');
-  mensagemElement.innerText = mensagem;
-  mensagensContainer.append(mensagemElement);
-}
-
-function adicionarJogador(nome) {
-  const jogadorElement = document.createElement('li');
-  jogadorElement.innerText = nome;
-  listaJogadores.append(jogadorElement);
-}
-
-function removerJogador(nome) {
-  const jogadores = listaJogadores.getElementsByTagName('li');
-  for (let i = 0; i < jogadores.length; i++) {
-    if (jogadores[i].innerText === nome) {
-      jogadores[i].remove();
-    }
+// Inicializa o Firebase
+const config = {
+    // Sua configuração do Firebase aqui
+  };
+  firebase.initializeApp(config);
+  
+  // Referência ao banco de dados
+  const dbRef = firebase.database().ref();
+  
+  // Cria uma nova partida
+  function criarPartida() {
+    const modoJogo = document.querySelector('input[name="modoJogo"]:checked').value;
+  
+    // Adiciona a nova partida ao banco de dados
+    const newPartidaRef = dbRef.child('partidas').push();
+    newPartidaRef.set({
+      modoJogo: modoJogo
+    });
+  
+    // Redireciona para a página da partida com o ID da partida
+    const idPartida = newPartidaRef.key;
+    window.location.href = "/partida/" + idPartida;
   }
-}
-
-nomeInput.addEventListener('change', (e) => {
-  nome = e.target.value;
-});
-
-enviarBtn.addEventListener('click', () => {
-  const mensagem = mensagemInput.value;
-  if (mensagem) {
-    socket.emit('mensagem', { nome, mensagem, partidaId });
-    mensagemInput.value = '';
+  
+  // Entra em uma partida
+  function entrarPartida() {
+    const idPartida = document.querySelector('#idPartida').value;
+  
+    // Verifica se a partida existe no banco de dados
+    dbRef.child('partidas').child(idPartida).get().then((snapshot) => {
+      if (snapshot.exists()) {
+        // Redireciona para a página da partida com o ID da partida
+        window.location.href = "/partida/" + idPartida;
+      } else {
+        alert("Partida não encontrada!");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
   }
-});
-
-socket.emit('entrarLobby', { nome, partidaId });
-
-socket.on('mensagem', (data) => {
-  adicionarMensagem(`${data.nome}: ${data.mensagem}`);
-});
-
-socket.on('jogadorEntrou', (data) => {
-  adicionarMensagem(`${data.nome} entrou na partida.`);
-  adicionarJogador(data.nome);
-});
-
-socket.on('jogadorSaiu', (data) => {
-  adicionarMensagem(`${data.nome} saiu da partida.`);
-  removerJogador(data.nome);
-});
-
-socket.on('listaJogadores', (data) => {
-  data.forEach((jogador) => {
-    adicionarJogador(jogador);
-  });
-});
-
-socket.on('partidaInexistente', () => {
-  window.location.href = 'index.html';
-});
+  
